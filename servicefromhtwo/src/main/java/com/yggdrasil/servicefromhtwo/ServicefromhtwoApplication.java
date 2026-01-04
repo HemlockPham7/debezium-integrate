245c1d0ce7b7503ecb0e061cb38fd76ab846ba34
HemlockPham7 <london1952hoang@gmail.com>
2026-01-04 18:50:12 +0700

    added h2 database application connection

---
+++ servicefromhtwo/src/main/java/com/yggdrasil/servicefromhtwo/ServicefromhtwoApplication.java
@@ -0,0 +1,13 @@
+package com.yggdrasil.servicefromhtwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicefromhtwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicefromhtwoApplication.class, args);
+	}
+
+}
---
+++ servicefromhtwo/src/test/java/com/yggdrasil/servicefromhtwo/ServicefromhtwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yggdrasil.servicefromhtwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicefromhtwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
