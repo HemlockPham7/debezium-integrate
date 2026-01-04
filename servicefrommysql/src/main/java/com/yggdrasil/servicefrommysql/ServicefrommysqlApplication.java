d4344d0ef1ad1005ade6191212bb9330cc736658
HemlockPham7 <london1952hoang@gmail.com>
2026-01-04 18:38:46 +0700

    Initialize project

---
+++ servicefrommysql/src/main/java/com/yggdrasil/servicefrommysql/ServicefrommysqlApplication.java
@@ -0,0 +1,13 @@
+package com.yggdrasil.servicefrommysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicefrommysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicefrommysqlApplication.class, args);
+	}
+
+}
